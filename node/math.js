c14566878f2e7b47a2e98473a1279f98472ccabf
shivam70170 <shivamsharma70170@gmail.com>
2026-08-08 15:23:21 +0530

    learn difference require btw import,make our package.json and learn import use

--- node/math.js
+++ node/math.js
@@ -1,21 +1,26 @@
-// functions
-function sum(a,b){
-    return a + b;
-}
+// // functions
+// function sum(a,b){
+//     return a + b;
+// }
 
-// ARROW FUNCTIONS
-let mul = (a,b) => {
-    return a*b;
-}
+// // ARROW FUNCTIONS
+// let mul = (a,b) => {
+//     return a*b;
+// }
 
-const g = 9.8;
-const PI = 3.14;
+// const g = 9.8;
+// const PI = 3.14;
 
-let obj = {
-    sum: sum,
-    mul: mul,
-    g: g,
-    PI: PI
-};
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI
+// };
 
-module.exports = obj;
+// module.exports = obj;
+
+export const sum = (a,b) => {return a + b};
+export const mul = (a,b) => {return a * b};
+export const g = 9.8;
+export const PI = 3.14;
--- node/script.js
+++ node/script.js
@@ -8,14 +8,19 @@
 
 // console.log("again hello");
 
-let args = process.argv;
-for(let i = 2; i < args.length; i++){
-    console.log(" hello to ", args[i]);
-}
+// let args = process.argv;
+// for(let i = 2; i < args.length; i++){
+//     console.log(" hello to ", args[i]);
+// }
 
-// ## module expotrs mean one file to another file
+// // ## module expotrs mean one file to another file
 
-const math = require("./math");
+// const math = require("./math");
 
-console.log(math.sum(3,5));
-console.log(math.PI);
+// console.log(math.sum(3,5));
+// console.log(math.PI);
+
+import {sum , PI} from "./math.js";
+
+console.log(sum(1,6));
+console.log(PI);
