09f1c2f47932ba8041b65a5f2da2e0ed17714c7d
shivam70170 <shivamsharma70170@gmail.com>
2026-08-09 21:58:08 +0530

    learn about ejs , using it ,views directory ,interpolation syntax and passing data ejs.

---
+++ EJSDIR/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+const port = 8080;
+app.listen(port,()=>{
+    console.log(`listen from port : ${port}`);
+})
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/",(req,res) => {
+    res.render("home");
+});
+
+app.get("/rolldice",(req,res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs",{num: diceVal});
+});
