eeedc37060c7edd3d44559027cbcf70f0cf49cb1
shivam70170 <shivamsharma70170@gmail.com>
2026-08-17 18:12:23 +0530

    Implement user management features: add user, delete user, and update view templates

--- SQL_CLASS/index.js
+++ SQL_CLASS/index.js
@@ -6,8 +6,9 @@ const port = 8080;
 const path = require('path');
 const { error } = require("console");
 const methodOverride = require('method-override');
+const {v4 : uuidv4} = require('uuid');
 
-app.set("views", "ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended : true}));
@@ -100,6 +101,89 @@ app.patch("/user/:id", (req, res) => {
         res.send("some error here");
     }
 });
+
+app.get("/user/add", (req, res) => {
+    res.render("adduser.ejs");
+});
+
+// ADD USER ROUTE
+app.post("/user/add",(req,res) => {
+    let id = uuidv4();
+    console.log(req.body);
+    let {username, email, password} = req.body;
+    let q = `INSERT INTO user (id,username,email,password) VALUES (?,?,?,?)`;
+    try{
+         connection.query(q, [id, username, email, password], (err, result) => {
+        if(err) throw err;
+        res.redirect("/user");
+    })
+    }catch(err){
+        console.log("error",err);
+        res.send("error,err");
+    }
+});
+
+app.get("/user/:id/delete", (req, res) => {
+    let { id } = req.params;
+
+    let q = `SELECT * FROM user WHERE id = ?`;
+
+    connection.query(q, [id], (err, result) => {
+        if (err) {
+            console.log("error", err);
+            return res.send("some error issues");
+        }
+
+        let user = result[0];
+
+        if (!user) {
+            return res.send("user not found");
+        }
+
+        res.render("delete.ejs", { user });
+    });
+});
+
+app.delete("/user/:id/delete", (req, res) => {
+    let {id} = req.params;
+    let {password:formpass,  email:newmail} = req.body;
+    let q = `SELECT * FROM user WHERE id ='${id}'`;
+
+    try{
+        connection.query(q, (err, result) => {
+            if(err){
+                console.log("error",err);
+                return res.send("some error here");
+            }
+            let user = result[0];
+            console.log(user);
+
+            if(!user){
+                return res.send("user not found");
+            }
+            if(formpass != user.password){
+                return res.send("wrong password");
+            }
+            if(newmail != user.email){
+                return res.send("wrong email");
+            }
+            
+            let q2 = `DELETE FROM user WHERE id = ?`;
+
+            connection.query(q2, [id], (err, result) => {
+                if(err){
+                    console.log("error",err);
+                    return res.send("error in deleting");
+                }
+                res.redirect("/user");
+            })
+        });
+    }catch(err){
+        console.log("error", err);
+        res.send("some error here");
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`port listen ${port}`);
