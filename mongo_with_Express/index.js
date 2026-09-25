84b244f51e5ff14cd454fcaf989dfb00b824a964
shivam70170 <shivamsharma70170@gmail.com>
2026-09-25 09:40:55 +0530

    feat: implement chat management features with CRUD operations and add styling

--- mongo_with_Express/index.js
+++ mongo_with_Express/index.js
@@ -4,9 +4,13 @@ const port = 8080;
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 main()
 .then((res) => {
@@ -18,6 +22,70 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 
 }
+
+// # # # EDIT ROUTE 
+
+app.get("/chats/:id/edit",async (req , res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+});
+
+// # # # UPDATE ROUTE 
+
+app.put("/chats/:id",async(req,res) => {
+    let {id} = req.params;
+    let {msg : newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+        id,
+        {msg : newMsg},
+        {runValidators : true, new : true }
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+// # # # 
+
+app.delete("/chats/:id", async (req, res) => {
+    let{id} = req.params;
+    let deletedChat =await Chat.findByIdAndDelete(id);
+    res.redirect("/chats")
+});
+//  # # # NEW CHAT ROUTE 
+
+app.get("/chats/new",(req, res) => {
+    res.render("new.ejs");
+});
+
+//  # # # CREATE ROUTE 
+
+app.post ("/chats",(req, res) => {
+    let { from, to, msg} = req.body;
+    let newChat = new Chat({
+        from : from,
+        to : to,
+        msg : msg,
+        created_at : new Date()
+    });
+
+    newChat
+    .save()
+    .then((res) => {
+        console.log("chat saved!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    res.redirect("/chats");
+});
+// # # # INDEX ROUTE 
+
+app.get("/chats",async (req, res) => {
+    let chats = await Chat.find();
+    // console.log(chats);
+    res.render("index.ejs",{chats});
+});
 
 // let Chat1 = new Chat({
 //     from : "neha",
@@ -37,7 +105,7 @@ async function main() {
 
 
 app.get("/",(req,res) => {
-    res.send("working port is fine.")
+    res.render("main.ejs");
 });
 
 app.listen(port,() => {
